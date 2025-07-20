7918d943cd8528b3b87d64fc3ee4457d8456c93d
Hlzw1 <318810862@qq.com>
2025-07-20 15:14:32 +0800

    joke_classmates

--- game/urls.py
+++ game/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from game.views import index, play
+from game.views import index, play, indexyyf, indexzq, indexlwx, indexlsy
 
 urlpatterns = [
-    path("", index, name = "index"),
+    path("", index,name = "index"),
+    path("classmates/yyfsb", indexyyf, name = "index1"),
+    path("classmates/zqsb", indexzq, name = "index2"),
+    path("classmates/lwxsb", indexlwx, name = "index3"),
+    path("classmates/lsysb", indexlsy, name = "index4"),
     path("play/", play, name = "play"),
 ]
--- game/views.py
+++ game/views.py
@@ -1,14 +1,39 @@
 from django.http import HttpResponse
 
 def index(resquest):
-    line1 = '<h1 style="text-align:center">叶研凤是个杀软</h1>'
+    line1 = '<h1 style="text-align:center">游戏页面</h1>'
     line2 = '<hr>'
     line4 = '<a href="play/">进入游戏页面</a>'
-    line3 = '<img src="" width=2000>'
-    return HttpResponse(line1 +line2 + line3)
+    line3 = '<img src="https://picx.zhimg.com/80/v2-c0ba9958a5bdc140c1f5f2af325cfbda_720w.png" width=1000 style="display: block; margin: 0 auto;">'
+    return HttpResponse(line1 +line2)
 
 def play(resquest):
     line1 = '<h1 style="text-align:center">游戏界面</h1>'
     line2 = '<a href="/yyfsb">回到菜单</a>'
     line3 = '<hr>'
     return HttpResponse(line1 + line2 + line3)
+
+def indexyyf(resquest):
+    line1 = '<h1 style="text-align:center">叶妍凤-"杀软"</h1>'
+    line2 = '<hr>'
+    line3 = '<img src="https://picx.zhimg.com/80/v2-c0ba9958a5bdc140c1f5f2af325cfbda_720w.png" width=1000 style="display: block; margin: 0 auto;">'
+    return HttpResponse(line1 + line2 + line3)
+
+def indexzq(resquest):
+    line1 = '<h1 style="text-align:center">周虔-"数学天才"</h1>'
+    line2 = '<hr>'
+    line3 = '<img src="https://pica.zhimg.com/80/v2-bc1e7d66505170f2c294d1ba78ef4d0c_720w.jpeg" width=1000 style="display: block; margin: 0 auto;">'
+    return HttpResponse(line1 + line2 + line3)
+
+def indexlwx(resquest):
+    line1 = '<h1 style="text-align:center">刘卫轩-"传奇插班生"</h1>'
+    line2 = '<hr>'
+    line3 = '<img src="https://pic1.zhimg.com/80/v2-7d36be99ca9546f777a9363c3206b661_720w.jpeg" width=1000 style="display: block; margin: 0 auto;">'
+    return HttpResponse(line1 + line2 + line3)
+
+def indexlsy(resquest):
+    line1 = '<h1 style="text-align:center">牛逼廖思懿</h1>'
+    line2 = '<hr>'
+    line3 = '<img src="" width=1000 style="display: block; margin: 0 auto;">'
+    line3 = '<img src="https://picx.zhimg.com/80/v2-29ecb94a1fa31b540f68a0ebeb4d8b5b_720w.jpeg" width=1000 style="display: block; margin: 0 auto;">'
+    return HttpResponse(line1 + line2 + line3) 
